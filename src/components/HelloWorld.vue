e421289963041820a823089a9ae7ee0e437a267a
xingchaofei <xingchaofei@wondertek.com.cn>
2020-07-14 17:43:30 +0800

    跑马灯效果

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="marquee">
+      <ul>
+        <li v-for="(item,index) in colors.concat(colors)" :key="index" :style="{background: item.color}">
+          {{ item.id }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -35,24 +16,61 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      colors: [{
+        color: '#E65D6E',
+        id: '1'
+      },{
+        color: '#30B08F',
+        id: '2'
+      },{
+        color: '#4AB7BD',
+        id: '3'
+      },{
+        color: '#FEC171',
+        id: '4'
+      },{
+        color: '#1890ff',
+        id: '5'
+      }]
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .marquee{
+    width: 1000px;
+    height: 200px;
+    margin: 20px auto;
+    overflow: hidden;
+  }
+  ul{
+    margin: 0;
+    padding: 0;
+    width: 200%;
+    height: 100%;
+    list-style: none;
+    animation: carousel 10s linear infinite;
+    position: relative;
+  }
+  ul li{
+    float: left;
+    width: 200px;
+    height: 100%;
+    text-align: center;
+    line-height: 200px;
+    font-size: 30px;
+  }
+  @keyframes carousel {
+    from {
+      left: 0;
+    }
+    to {
+      left: -1000px;
+    }
+  }
 </style>
